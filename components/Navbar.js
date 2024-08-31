282ec444c531607fab922a00ef32da74b7b94ec3
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-08-31 12:47:16 +0530

    Updates: Footer and Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='flex justify-between'>
+            <div className="logo">
+                <Image src='/Logo.png' alt='' height={60} width={60} />
+            </div>
+            <div className="nav">
+                <ul className='flex item-center'>
+                    <li>Tshirts</li>
+                    <li>Hoodies</li>
+                    <li>Stickers</li>
+                    <li>Mugs</li>
+                </ul>
+            </div>
+            <div>
+                <div className="cart">
+                    <button>Cart</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
