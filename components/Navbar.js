a9800c43fb6bda2b39ed19cc85de25c9304e536c
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-09-06 22:55:42 +0530

    Update: Sidebar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,24 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useRef } from 'react'
 import { MdShoppingCart } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+
+    const ref = useRef()
+
+    const toggleCart = () => {
+        if (ref.current.classList.contains('translate-x-full')) {
+            ref.current.classList.remove('translate-x-full')
+            ref.current.classList.add('translate-x-0')
+        }
+        else if (!ref.current.classList.contains('translate-x-full')) {
+            ref.current.classList.remove('translate-x-0')
+            ref.current.classList.add('translate-x-full')
+        }
+    }
+
     return (
         <div className='flex flex-col md:flex-row justify-between md:justify-between items-center px-2 shadow-md'>
             <div className="logo mx-5">
@@ -29,10 +44,21 @@ const Navbar = () => {
             </div>
             <div>
                 <div className="cart">
-                    <button>
+                    <button onClick={toggleCart}>
                         <MdShoppingCart className='text-xl md:text-3xl' />
                     </button>
                 </div>
+            </div>
+            <div ref={ref} className="sideCart absolute top-0 right-0 bg-blue-100 p-10 transform transition-transform translate-x-full">
+                <h2 className="font-bold text-xl">Shopping Cart</h2>
+                <span onClick={toggleCart} className="absolute right-3 top-5 text-3xl text-blue-500 cursor-pointer">
+                    <IoClose />
+                </span>
+                <ol>
+                    <li>
+                        <span>TShirts - Wear the code</span>
+                    </li>
+                </ol>
             </div>
         </div>
     )
