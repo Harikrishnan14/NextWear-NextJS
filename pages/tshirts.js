a2986e40cace960eac7a259c74b64e42f41b7d97
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-03-06 21:48:44 +0530

    Update: tshirts page

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -5,7 +5,7 @@ const Tshirts = () => {
     return (
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-wrap -m-4">
+                <div className="flex flex-wrap -m-4 justify-center">
                     <Link href={'/product/wear-the-code'} className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-8">
                         <div>
                             <div className="block relative rounded overflow-hidden">
