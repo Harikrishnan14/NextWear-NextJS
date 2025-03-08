ad616bd62809e134baece7a444b716d3491fad5a
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-03-08 22:48:08 +0530

    Update: login page redirection from Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,7 @@ import React, { useRef } from 'react'
 import { MdShoppingCart } from "react-icons/md";
 import { IoClose, IoBagCheck } from "react-icons/io5";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
+import { MdAccountCircle } from "react-icons/md";
 
 const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
 
@@ -44,10 +45,11 @@ const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
                 </ul>
             </div>
             <div>
-                <div className="cart">
-                    <button onClick={toggleCart}>
-                        <MdShoppingCart className='text-xl md:text-3xl' />
-                    </button>
+                <div className="cursor-pointer cart absolute right-0 top-4 mx-5 flex">
+                    <Link href='/login'>
+                        <MdAccountCircle className='text-xl md:text-3xl me-3' />
+                    </Link>
+                    <MdShoppingCart className='text-xl md:text-3xl' onClick={toggleCart} />
                 </div>
             </div>
             <div ref={ref} className={`sideCart absolute top-0 right-0 bg-blue-100 px-8 py-10 transform transition-transform ${Object.keys(cart).length !== 0 ? 'translate-x-0' : 'translate-x-full'} w-72 h-[100vh]`}>
