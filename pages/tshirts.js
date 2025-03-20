4026522437cf899981afa3502325720930fafe5b
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-03-20 21:53:26 +0530

    Update: tshirts fetching logic

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -8,16 +8,16 @@ const Tshirts = ({ products }) => {
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-wrap -m-4 justify-center">
-                    {products?.map((item, index) => (
-                        <Link passHref={true} href={`/product/${item?.slug}`} className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-8" key={index}>
+                    {Object.keys(products)?.map((item, index) => (
+                        <Link passHref={true} href={`/product/${products[item]?.slug}`} className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-8" key={index}>
                             <div>
                                 <div className="block relative rounded overflow-hidden">
-                                    <img alt="tshirt" className="object-cover w-full object-center md:h-[50vh] block" src={item?.img} />
+                                    <img alt="tshirt" className="object-cover w-full object-center md:h-[50vh] block" src={products[item]?.img} />
                                 </div>
                                 <div className="mt-4">
                                     <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">{item?.title}</h2>
-                                    <p className="mt-1">₹{item?.price}</p>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">{products[item]?.title}</h2>
+                                    <p className="mt-1">₹{products[item]?.price}</p>
                                     <p className="mt-1">S, M, L XL, XXL</p>
                                 </div>
                             </div>
@@ -34,9 +34,26 @@ export async function getServerSideProps() {
         await mongoose.connect(process.env.MONGO_URI)
     }
     let products = await Product.find({ category: 'tshirt' })
+    let tshirts = {}
+    for (let item of products) {
+        if (item.title in tshirts) {
+            if (!tshirts[item.title].color.includes(item.color) && item.availableQty > 0) {
+                tshirts[item.title].color.push(item.color)
+            }
+            if (!tshirts[item.title].size.includes(item.size) && item.availableQty > 0) {
+                tshirts[item.title].size.push(item.size)
+            }
+        } else {
+            tshirts[item.title] = JSON.parse(JSON.stringify(item))
+            if (item.availableQty > 0) {
+                tshirts[item.title].color = [item.color]
+                tshirts[item.title].size = [item.size]
+            }
+        }
+    }
 
     return {
-        props: { products: JSON.parse(JSON.stringify(products)) }
+        props: { products: JSON.parse(JSON.stringify(tshirts)) }
     };
 }
 
