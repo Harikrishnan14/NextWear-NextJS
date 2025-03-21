19d5b3c9925c4d8d0514a1b0642d7c7dec2097d5
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-03-21 16:48:31 +0530

    Update: displaying available size and colors

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -18,7 +18,20 @@ const Tshirts = ({ products }) => {
                                     <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
                                     <h2 className="text-gray-900 title-font text-lg font-medium">{products[item]?.title}</h2>
                                     <p className="mt-1">₹{products[item]?.price}</p>
-                                    <p className="mt-1">S, M, L XL, XXL</p>
+                                    <div className="mt-1">
+                                        {products[item].size.includes('S') && <span className='border border-gray-300 px-1 mx-1'>S</span>}
+                                        {products[item].size.includes('M') && <span className='border border-gray-300 px-1 mx-1'>M</span>}
+                                        {products[item].size.includes('L') && <span className='border border-gray-300 px-1 mx-1'>L</span>}
+                                        {products[item].size.includes('XL') && <span className='border border-gray-300 px-1 mx-1'>XL</span>}
+                                        {products[item].size.includes('XXL') && <span className='border border-gray-300 px-1 mx-1'>XXL</span>}
+                                    </div>
+                                    <div className="mt-1">
+                                        {products[item].color.includes('black') && <button className="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
+                                        {products[item].color.includes('blue') && <button className="border-2 border-gray-300 ml-1 bg-blue-800 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                        {products[item].color.includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-800 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                        {products[item].color.includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-800 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                        {products[item].color.includes('purple') && <button className="border-2 border-gray-300 ml-1 bg-purple-800 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                    </div>
                                 </div>
                             </div>
                         </Link>
