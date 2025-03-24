f62f83e6ea1ca88ac60670fdccb7e29e87df0eed
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-03-24 22:02:31 +0530

    Update: displaying available colors

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,6 +8,8 @@ const Slug = ({ addToCart, variants, product }) => {
     const { slug } = router.query
     const [pin, setPin] = useState()
     const [available, setAvailable] = useState(null)
+    const [color, setColor] = useState(product.color)
+    const [size, setSize] = useState(product.size)
 
     const checkAvailability = async () => {
         let pins = await fetch('http://localhost:3000/api/pincode')
@@ -76,18 +78,23 @@ const Slug = ({ addToCart, variants, product }) => {
                             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                                 <div className="flex">
                                     <span className="mr-3">Color</span>
-                                    <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                                    <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                                    <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
+                                    {Object.keys(variants).includes('white') && <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                    {Object.keys(variants).includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                    {Object.keys(variants).includes('black') && <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                    {Object.keys(variants).includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                    {Object.keys(variants).includes('blue') && <button className="border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                    {Object.keys(variants).includes('purple') && <button className="border-2 border-gray-300 ml-1 bg-purple-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                    {Object.keys(variants).includes('yellow') && <button className="border-2 border-gray-300 ml-1 bg-yellow-500 rounded-full w-6 h-6 focus:outline-none"></button>}
                                 </div>
                                 <div className="flex ml-6 items-center">
                                     <span className="mr-3">Size</span>
                                     <div className="relative">
                                         <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                                            <option>SM</option>
+                                            <option>S</option>
                                             <option>M</option>
                                             <option>L</option>
                                             <option>XL</option>
+                                            <option>XXL</option>
                                         </select>
                                         <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                                             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
