ed5e569cd2e60ba5bae41c28c5042b880c0e0e43
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-03-25 23:52:23 +0530

    Update: commented unwanted items

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -43,7 +43,8 @@ const Slug = ({ addToCart, variants, product }) => {
                             <h2 className="text-sm title-font text-gray-500 tracking-widest">NEXTWEAR</h2>
                             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.title} ({product.size}/{product.color})</h1>
                             <div className="flex mb-4">
-                                <span className="flex items-center">
+
+                                {/* <span className="flex items-center">
                                     <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                                         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                                     </svg>
@@ -77,7 +78,8 @@ const Slug = ({ addToCart, variants, product }) => {
                                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                                         </svg>
                                     </a>
-                                </span>
+                                </span> */}
+
                             </div>
                             <p className="leading-relaxed">{product.desc}</p>
                             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
@@ -124,11 +126,11 @@ const Slug = ({ addToCart, variants, product }) => {
                                 >
                                     Add to Cart
                                 </button>
-                                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                                {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                     <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                                     </svg>
-                                </button>
+                                </button> */}
                             </div>
                             <div className="pin mt-6 flex space-x-2 text-sm">
                                 <input type="text" placeholder="Enter your Pincode" onChange={(e) => onChangePin(e)} className="px-2 border-2 border-gray-200 rounded-md" />
