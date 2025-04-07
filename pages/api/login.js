25889de2c8e2be325fbcaf00bda9f3315d824cb3
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-04-07 23:52:18 +0530

    Update JWT setup

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,15 +1,18 @@
 import connectDb from "@/middleware/mongoose";
 import User from "@/models/User";
 var CryptoJS = require("crypto-js");
+var jwt = require('jsonwebtoken');
 
 const handler = async (req, res) => {
     if (req.method == 'POST') {
         let user = await User.findOne({ email: req.body.email })
+        console.log(user);
         const bytes = CryptoJS.AES.decrypt(user.password, 'secret key 123');
         let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
         if (user) {
             if (req.body.email === user.email && req.body.password === decryptedPass) {
-                res.status(200).json({ success: true, email: user.email, name: user.name })
+                var token = jwt.sign({ success: true, email: user.email, name: user.name }, 'JWTSecret');
+                res.status(200).json({token})
             } else {
                 res.status(200).json({ success: false, error: "Invalid credentials!" })
             }
