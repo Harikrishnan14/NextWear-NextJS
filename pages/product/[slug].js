42bc47b1e9662461e1e956836956bb53fad84621
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-04-21 21:56:52 +0530

    Update: api url added to .env

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,7 +13,7 @@ const Slug = ({ addToCart, buyNow, variants, product }) => {
     const [size, setSize] = useState(product.size)
 
     const checkAvailability = async () => {
-        let pins = await fetch('http://localhost:3000/api/pincode')
+        let pins = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pincode`)
         let pinJson = await pins.json()
         if (pinJson.includes(parseInt(pin))) {
             setAvailable(true)
@@ -49,7 +49,7 @@ const Slug = ({ addToCart, buyNow, variants, product }) => {
     }
 
     const refreshVariants = (newColor, newSize) => {
-        let url = `http://localhost:3000/product/${variants[newColor][newSize]['slug']}`
+        let url = `${process.env.NEXT_PUBLIC_HOST}/product/${variants[newColor][newSize]['slug']}`
         window.location = url
     }
 
