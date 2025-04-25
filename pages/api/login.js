7390b4787797bcf2b85ae0aa0b875e829dbc3854
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-04-25 23:25:11 +0530

    Update: jwt and aes secret keys changed

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,11 +6,11 @@ var jwt = require('jsonwebtoken');
 const handler = async (req, res) => {
     if (req.method == 'POST') {
         let user = await User.findOne({ email: req.body.email })
-        const bytes = CryptoJS.AES.decrypt(user.password, 'secret key 123');
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.AES_SECRET);
         let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
         if (user) {
             if (req.body.email === user.email && req.body.password === decryptedPass) {
-                var token = jwt.sign({ email: user.email, name: user.name }, 'JWTSecret', { expiresIn: "2d"});
+                var token = jwt.sign({ email: user.email, name: user.name }, process.env.JWT_SECRET, { expiresIn: "2d"});
                 res.status(200).json({ success: true, token })
             } else {
                 res.status(200).json({ success: false, error: "Invalid credentials!" })
