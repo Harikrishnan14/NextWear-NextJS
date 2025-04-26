66ff324cc2fde08957a68c70d1f094973de1e4df
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-04-26 22:00:16 +0530

    Fix: navbar not showing properlly

--- pages/_app.js
+++ pages/_app.js
@@ -86,8 +86,8 @@ export default function App({ Component, pageProps }) {
     const token = localStorage.getItem('token')
     if (token) {
       setUser({ value: token })
-      setKey(Math.random())
     }
+    setKey(Math.random())
   }, [router.query])
 
   return <>
