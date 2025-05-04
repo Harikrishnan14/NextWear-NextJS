5ee34b12053ea91c41f0f76871a28f4cb1b12c32
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-05-04 22:55:02 +0530

    Update: redirection

--- pages/api/posttransaction.js
+++ pages/api/posttransaction.js
@@ -6,15 +6,15 @@ const handler = async (req, res) => {
 
     // Update status into Orders table after checking the transaction status
     if (req.body.STATUS === 'TXN_SUCCESS') {
-        Order.findOneAndUpdate({ orderId: req.body.ORDERID }, { status: 'Paid', paymentInfo: JSON.stringify(req.body) })
+        let order = Order.findOneAndUpdate({ orderId: req.body.ORDERID }, { status: 'Paid', paymentInfo: JSON.stringify(req.body) })
     } else if (req.body.STATUS === 'PENDING') {
-        Order.findOneAndUpdate({ orderId: req.body.ORDERID }, { status: 'Pending', paymentInfo: JSON.stringify(req.body) })
+        let order = Order.findOneAndUpdate({ orderId: req.body.ORDERID }, { status: 'Pending', paymentInfo: JSON.stringify(req.body) })
     }
 
     /// Initiate Shipping
 
     // Redirect user to the order confirmation page
-    res.redirect('/order', 200)
+    res.redirect('/order?id=' + order._id, 200)
 
 
     // res.status(200).json({ body: req.body });
