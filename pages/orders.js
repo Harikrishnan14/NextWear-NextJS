d4902be41e20baed8c5d5eff371d3899de186370
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-05-06 22:13:33 +0530

    Update: displaying data on orders page

--- pages/orders.js
+++ pages/orders.js
@@ -1,6 +1,10 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 
 const orders = () => {
+    const router = useRouter()
+    const [orders, setOrders] = useState([])
 
     useEffect(() => {
         const fetchOrders = async () => {
@@ -12,6 +16,7 @@ const orders = () => {
                 body: JSON.stringify({ token: localStorage.getItem("token") })
             })
             let res = await a.json()
+            setOrders(res.orders);
         }
 
         if (!localStorage.getItem('token')) {
@@ -34,31 +39,23 @@ const orders = () => {
                                     <thead
                                         className="border-b border-neutral-200 font-medium dark:border-black/10 text-black">
                                         <tr>
-                                            <th scope="col" className="px-6 py-4">#</th>
-                                            <th scope="col" className="px-6 py-4">First</th>
-                                            <th scope="col" className="px-6 py-4">Last</th>
-                                            <th scope="col" className="px-6 py-4">Handle</th>
+                                            <th scope="col" className="px-6 py-4">OID</th>
+                                            <th scope="col" className="px-6 py-4">Email</th>
+                                            <th scope="col" className="px-6 py-4">Amount</th>
+                                            <th scope="col" className="px-6 py-4">Details</th>
                                         </tr>
                                     </thead>
                                     <tbody className='text-black'>
-                                        <tr className="border-b border-neutral-200 dark:border-black/10">
-                                            <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
-                                            <td className="whitespace-nowrap px-6 py-4">Mark</td>
-                                            <td className="whitespace-nowrap px-6 py-4">Otto</td>
-                                            <td className="whitespace-nowrap px-6 py-4">@mdo</td>
-                                        </tr>
-                                        <tr className="border-b border-neutral-200 dark:border-black/10">
-                                            <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
-                                            <td className="whitespace-nowrap px-6 py-4">Jacob</td>
-                                            <td className="whitespace-nowrap px-6 py-4">Thornton</td>
-                                            <td className="whitespace-nowrap px-6 py-4">@fat</td>
-                                        </tr>
-                                        <tr className="border-b border-neutral-200 dark:border-black/10">
-                                            <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
-                                            <td className="whitespace-nowrap px-6 py-4">Larry</td>
-                                            <td className="whitespace-nowrap px-6 py-4">Wild</td>
-                                            <td className="whitespace-nowrap px-6 py-4">@twitter</td>
-                                        </tr>
+                                        {orders.map((item, index) => (
+                                            <tr className="border-b border-neutral-200 dark:border-black/10" key={index}>
+                                                <td className="whitespace-nowrap px-6 py-4 font-medium">{item.orderId}</td>
+                                                <td className="whitespace-nowrap px-6 py-4">{item.email}</td>
+                                                <td className="whitespace-nowrap px-6 py-4">{item.amount}</td>
+                                                <td className="whitespace-nowrap px-6 py-4">
+                                                    <Link href={'/order?id=' + item.orderId}>Details</Link>
+                                                </td>
+                                            </tr>
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
