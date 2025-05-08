d7596befb7dc0f869f0e8c623f28d2f68ffd5adb
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-05-08 20:48:11 +0530

    Fix: defining order variable outside if statement

--- pages/api/posttransaction.js
+++ pages/api/posttransaction.js
@@ -2,13 +2,14 @@ import connectDb from "@/middleware/mongoose";
 import Order from "@/models/Order";
 
 const handler = async (req, res) => {
+    let order;
     // TODO : Validate paytm checksum
 
     // Update status into Orders table after checking the transaction status
     if (req.body.STATUS === 'TXN_SUCCESS') {
-        let order = Order.findOneAndUpdate({ orderId: req.body.ORDERID }, { status: 'Paid', paymentInfo: JSON.stringify(req.body) })
+        order = Order.findOneAndUpdate({ orderId: req.body.ORDERID }, { status: 'Paid', paymentInfo: JSON.stringify(req.body) })
     } else if (req.body.STATUS === 'PENDING') {
-        let order = Order.findOneAndUpdate({ orderId: req.body.ORDERID }, { status: 'Pending', paymentInfo: JSON.stringify(req.body) })
+        order = Order.findOneAndUpdate({ orderId: req.body.ORDERID }, { status: 'Pending', paymentInfo: JSON.stringify(req.body) })
     }
 
     /// Initiate Shipping
