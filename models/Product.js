9378235f365b155c900c071a9106d30609fa19fa
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-06-26 18:37:02 +0530

    Update: default image value for products updated

--- models/Product.js
+++ models/Product.js
@@ -17,6 +17,7 @@ const ProductSchema = new mongoose.Schema({
     img: {
         type: String,
         required: true,
+        default: "https://mrmockup.com/wp-content/uploads/2024/07/Free-Backside-T-Shirt-Mockup-Square-1024x1024.jpg?x25494"
     },
     category: {
         type: String,
